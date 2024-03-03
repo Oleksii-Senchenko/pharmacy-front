3896340f8bff2b53cd686f5902013b54a0689846
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2024-03-03 08:23:32 +0200

    Update operations.js

--- src/redux/pharmacy/operations.js
+++ src/redux/pharmacy/operations.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "http://localhost:3001/api";
+axios.defaults.baseURL = "https://pharmacy-api-k1ig.onrender.com/api";
+// axios.defaults.baseURL = "http://localhost:3001/api";
 
 export const getPharmacies = createAsyncThunk(
   "pharmacy/get",
