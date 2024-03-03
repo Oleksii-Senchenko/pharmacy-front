842e354588e77b58f3f32eb20e8a494913057341
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2024-03-03 07:26:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/pharmacy-front">
       <Provider store={store}>
         <App />
       </Provider>
